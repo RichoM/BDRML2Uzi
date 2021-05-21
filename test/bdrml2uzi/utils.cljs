d6ac851f28eacfdcf3d82cc98b3b887079d80784
RichoM <richi.moran@gmail.com>
2021-05-21 20:29:59 -0300

    Refactoring. Simplified code for the equivalent? functions

--- test/bdrml2uzi/utils.cljs
+++ test/bdrml2uzi/utils.cljs
@@ -1,42 +1,18 @@
 (ns bdrml2uzi.utils)
 
-(comment
- (def pred (fn [a] (println a)
-             (odd? a)))
-
-(def s #{1 2 3 4 5 6})
-(contains? s 1)
-(every? number? s)
- ,,)
-
 (declare equivalent?)
 
 (defn- equivalent-sequential? [a b]
   (when (sequential? b)
-    (try
-      (let [limit (count a)]
-        (loop [i 0]
-          (if (< i limit)
-            (if (equivalent? (nth a i)
-                             (nth b i))
-              (recur (inc i))
-              false)
-            true)))
-      (catch js/Object _ false))))
+    (every? (fn [[i v]]
+              (equivalent? v (nth b i)))
+            (map-indexed (fn [i e] [i e]) a))))
 
 (defn- equivalent-map? [a b]
   (when (associative? b)
-    (let [keys* (keys a)]
-      (loop [key (first keys*)
-             rest (next keys*)]
-        (if (equivalent? (get a key)
-                         (get b key))
-          (if rest
-            (recur
-              (first rest)
-              (next rest))
-            true)
-          false)))))
+    (every? (fn [[k v]]
+              (equivalent? v (get b k)))
+            a)))
 
 (defn- equivalent-set? [a b]
   (every? (fn [v]
@@ -46,7 +22,8 @@
           a))
 
 (defn equivalent? [a b]
-  (or (= a b)
+  (or (identical? a b)
+      (= a b)
       (cond
         (sequential? a) (equivalent-sequential? a b)
         (map? a) (equivalent-map? a b)
