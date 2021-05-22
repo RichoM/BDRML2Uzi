aebcd194646765111730d6c1bddabb724570b18e
RichoM <richi.moran@gmail.com>
2021-05-22 12:18:53 -0300

    Added the code generation to the demo

--- src/bdrml2uzi/core.cljs
+++ src/bdrml2uzi/core.cljs
@@ -1,8 +1,14 @@
 (ns bdrml2uzi.core
-  (:require [bdrml2uzi.parser :as p]))
+  (:require [bdrml2uzi.parser :as p]
+            [bdrml2uzi.codegen :as cg]))
 
 (defn init []
   (println "Hello World"))
 
 (defn ^:export parse [src]
   (clj->js (p/parse src)))
+
+(defn ^:export codegen [src]
+  (-> src
+      p/parse
+      cg/generate-code))
